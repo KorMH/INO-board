540ce680bd83180b5706b7564868a859af9b2825
MyungHyun Lee <dvkk12@gmail.com>
2023-06-27 10:34:09 +0900

    first commit

---
+++ src/test/java/com/board/inoboard/InoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.inoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
